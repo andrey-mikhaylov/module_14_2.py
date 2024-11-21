ddc6033381d020250fc9cf454c673d9679a4e344
Andrey <andrey.mikhaylov.n@gmail.com>
2024-11-21 13:23:06 +0300

    create, fill

--- module_14_1.py
+++ module_14_1.py
@@ -1,54 +1,89 @@
 import sqlite3
+from sqlite3 import Connection as Db
 
 
-def create_db():
-    # Создайте файл базы данных not_telegram.db и подключитесь к ней, используя встроенную библиотеку sqlite3.
-    ...
-
-def create_table():
-    # Создайте объект курсора и выполните следующие действия при помощи SQL запросов:
-    # Создайте таблицу Users, если она ещё не создана. В этой таблице должны присутствовать следующие поля:
-    # id - целое число, первичный ключ
-    # username - текст (не пустой)
-    # email - текст (не пустой)
-    # age - целое число
-    # balance - целое число (не пустой)
-    ...
+def create_db(database_name: str):
+    # Создайте файл базы данных not_telegram.db
+    # и подключитесь к ней, используя встроенную библиотеку sqlite3.
+    db = sqlite3.connect(database_name)
+    return db
 
 
-def fill_table():
+def create_table(db: Db, table: str):
+    # Создайте объект курсора
+    cursor = db.cursor()
+    # Создайте таблицу Users, если она ещё не создана.
+    cursor.execute(f'CREATE TABLE IF NOT EXISTS {table} ('
+        # В этой таблице должны присутствовать следующие поля:
+        'id INTEGER PRIMARY KEY,'   # целое число, первичный ключ
+        'username TEXT NOT NULL,'   # текст (не пустой)
+        'email TEXT NOT NULL,'      # текст (не пустой)
+        'age INTEGER,'              # целое число
+        'balance INTEGER NOT NULL'  # целое число (не пустой)
+    ')')
+
+    cursor.execute(f'DELETE FROM {table}')
+
+    #cursor.execute('CREATE INDEX IF NOT EXISTS idx_email ON Users (email)')
+
+
+def fill_table(db: Db, table: str):
+    cursor = db.cursor()
     # Заполните её 10 записями:
-    # User1, example1@gmail.com, 10, 1000
-    # User2, example2@gmail.com, 20, 1000
-    # User3, example3@gmail.com, 30, 1000
-    # ...
-    # User10, example10@gmail.com, 100, 1000
+    for i in range(1, 11):
+        cmd = f'INSERT INTO {table} (username, email, age, balance) VALUES (?, ?, ?, ?)'
+        data = f'User{i}', f'example{i}@gmail.com', str(i*10), '1000'
+        print(cmd, ',', data)
+        cursor.execute(cmd, data)
+
+
+def modify_table(db, table: str):
+    cursor = db.cursor()
     # Обновите balance у каждой 2ой записи начиная с 1ой на 500:
+    for i in range(1, 11, 2):
+        user = f'User{i}'
+        request = ''
+        print(user)
+        ...
     # User1, example1@gmail.com, 10, 500
     # User2, example2@gmail.com, 20, 1000
     # User3, example3@gmail.com, 30, 500
     # ...
     # User10, example10@gmail.com, 100, 1000
+
     # Удалите каждую 3ую запись в таблице начиная с 1ой:
+    for i in range(1, 11, 3):
+        user = f'User{i}'
+        print(user)
+        ...
     # User2, example2@gmail.com, 20, 1000
     # User3, example3@gmail.com, 30, 500
     # User5, example5@gmail.com, 50, 500
     # ...
     # User9, example9@gmail.com, 90, 500
-    ...
 
 
-def fetch_records():
+def fetch_records(db: Db, table: str):
+    cursor = db.cursor()
     # Сделайте выборку всех записей при помощи fetchall(), где возраст не равен 60 и выведите их в консоль в следующем формате (без id):
     # Имя: <username> | Почта: <email> | Возраст: <age> | Баланс: <balance>
-    return ''
+    return []
+
+
+def close_db(db: Db):
+    db.commit()
+    db.close()
 
 
 def main():
-    create_db()
-    create_table()
-    fill_table()
-    result = fetch_records()
+    db = create_db('not_telegram.db')
+    table = 'Users'
+    create_table(db, table)
+    fill_table(db, table)
+    modify_table(db, table)
+    result = fetch_records(db, table)
+    close_db(db)
+
     print(result)
     """
     Вывод на консоль:
