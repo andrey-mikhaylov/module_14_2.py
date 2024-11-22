7ec51fd72be5340b1d5e18e466ff464a00cf0f99
Andrey <andrey.mikhaylov.n@gmail.com>
2024-11-22 12:29:24 +0300

    done

--- module_14_2.py
+++ module_14_2.py
@@ -2,6 +2,8 @@ import sqlite3
 from sqlite3 import Connection as Db
 
 
+#-----------------------------------------------------------------------------------------------------------------------
+# database
 def create_db(database_name: str):
     # Создайте файл базы данных not_telegram.db
     # и подключитесь к ней, используя встроенную библиотеку sqlite3.
@@ -27,10 +29,43 @@ def create_table(db: Db, table: str):
         'balance INTEGER NOT NULL'  # целое число (не пустой)
     ')')
 
-    cursor.execute(f'DELETE FROM {table}')
+    delete_from_db(db, table)
     #cursor.execute('CREATE INDEX IF NOT EXISTS idx_email ON Users (email)')
 
 
+def delete_from_db(db: Db, table: str, cond: str = 'TRUE', value: tuple = ()):
+    cursor = db.cursor()
+    cmd = f'DELETE FROM {table} WHERE {cond}'
+    cursor.execute(cmd, value)
+
+
+def update_db(db: Db, table: str, op: str, cond: str, params: tuple):
+    cursor = db.cursor()
+    cmd = f'UPDATE {table} SET {op} WHERE {cond}'
+    cursor.execute(cmd, params)
+
+
+def fetch_records(db: Db, table: str, cond: str = 'TRUE', value: tuple = ()):
+    cursor = db.cursor()
+    cursor.execute(f'SELECT * FROM {table} WHERE {cond}', value)
+    return cursor.fetchall()
+
+
+def op_db(db: Db, table: str, op: str, cond: str = 'TRUE', value: tuple = ()):
+    cursor = db.cursor()
+    cursor.execute(f'SELECT {op} FROM {table} WHERE {cond}', value)
+    return cursor.fetchone()[0]
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+# print
+def print_records(records: list):
+    for _, username, email, age, balance in records:
+        print(f'Имя: {username} | Почта: {email} | Возраст: {age} | Баланс: {balance}')
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+# tests
 def fill_table(db: Db, table: str):
     cursor = db.cursor()
     # Заполните её 10 записями:
@@ -40,43 +75,40 @@ def fill_table(db: Db, table: str):
         cursor.execute(cmd, params)
 
 
-def delete_from_db(db, table: str, cond: str = 'TRUE', value: tuple = ()):
-    cursor = db.cursor()
-    cmd = f'DELETE FROM {table} WHERE {cond}'
-    cursor.execute(cmd, value)
-
-
-def modify_table(db, table: str):
-    cursor = db.cursor()
+def modify_table(db: Db, table: str):
     # Обновите balance у каждой 2ой записи начиная с 1ой на 500:
     for i in range(1, 11, 2):
-        cmd = f'UPDATE {table} SET balance = ? WHERE username = ?'
-        params = 500, f'User{i}'
-        cursor.execute(cmd, params)
+        update_db(db, table, 'balance = ?', 'username = ?', (500, f'User{i}'))
 
     # Удалите каждую 3ую запись в таблице начиная с 1ой:
     for i in range(1, 11, 3):
         delete_from_db(db, table, 'username = ?', (f'User{i}',))
 
 
-def fetch_records(db: Db, table: str, cond: str = 'TRUE', value: tuple = ()):
-    cursor = db.cursor()
+def test1(db: Db, table: str):
     # Сделайте выборку всех записей при помощи fetchall(), где возраст не равен 60
-    cursor.execute(f'SELECT * FROM {table} WHERE {cond}', value)
-    return cursor.fetchall()
+    results = fetch_records(db, table, 'age != ?', ('60',))
+#    results = fetch_records(db, table)
+    print_records(results)
 
 
-def avg_db(db: Db, table: str, param: str):
-    cursor = db.cursor()
-    cursor.execute(f'SELECT AVG({param}) FROM {table}')
-    return cursor.fetchone()[0]
+def test2(db: Db, table: str):
+    # Удалите из базы данных not_telegram.db запись с id = 6.
+    delete_from_db(db, table, 'id = ?', ('6',))
+    # Подсчитать общее количество записей.
+    records_count = op_db(db, table, 'COUNT(*)')
+    # Посчитать сумму всех балансов.
+    records_sum = op_db(db, table, 'SUM(balance)')
+    records_avg = records_sum / records_count
+    # Вывести в консоль средний баланс всех пользователей.
+    print(records_avg)
 
-
-def print_records(records: list):
-    # и выведите их в консоль в следующем формате (без id):
-    # Имя: <username> | Почта: <email> | Возраст: <age> | Баланс: <balance>
-    for _, username, email, age, balance in records:
-        print(f'Имя: {username} | Почта: {email} | Возраст: {age} | Баланс: {balance}')
+#    records_avg = op_db(db, table, 'AVG(balance)')
+#    print(records_avg)
+    """
+    Вывод на консоль:
+    700.0
+    """
 
 
 def main():
@@ -85,19 +117,9 @@ def main():
     create_table(db, table)
     fill_table(db, table)
     modify_table(db, table)
-#    results = fetch_records(db, table, 'age != ?', ('60',))
-#    results = fetch_records(db, table)
-#    print_records(results)
-    delete_from_db(db, table, 'id = ?', ('6',))
-#    print_records(fetch_all(db, table))
-    print(avg_db(db, table, 'balance'))
-
+#    test1(db, table)
+    test2(db, table)
     close_db(db)
-
-    """
-    Вывод на консоль:
-    700.0
-    """
 
 
 if __name__ == '__main__':
